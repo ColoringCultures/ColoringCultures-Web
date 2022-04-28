363799a390b2a944d772e4b05d4d298749c3d92b
qgatssdev <chivoomodu@gmail.com>
2022-04-28 19:20:32 +0100

    Login page finished

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,13 +6,16 @@ const Login = () => {
     <div className="root">
       <div className="color1">
         <h1>Login to your Account</h1>
+        <p>Welcome Boss, kindly input your details</p>
         <div className="color-flex">
           <input type="email" placeholder="Email Address" />
           <input type="password" placeholder="Password" />
           <button>Login</button>
         </div>
       </div>
-      <div className="color2"></div>
+      <div className="color2">
+        <img src={require('../../assets/2.png')} alt="" />
+      </div>
     </div>
   );
 };
