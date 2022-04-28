12587145263f1dd749c81a1841651084139affa2
qgatssdev <chivoomodu@gmail.com>
2022-04-28 17:35:46 +0100

    initial commit

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Login.scss';
+
+const Login = () => {
+  return (
+    <div className="root">
+      <div className="color1">
+        <h1>Login to your Account</h1>
+        <div className="color-flex">
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+        </div>
+      </div>
+      <div>
+        <h2>Login</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
