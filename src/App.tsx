b9701987ffbb2315285f10c123aa856f7034b501
qgatssdev <chivoomodu@gmail.com>
2022-04-29 13:16:57 +0100

    deleting files and building Dashboard root

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Dashboard.scss';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-root">
+      <div className="navbar">
+        <h1>Dashboard</h1>
+      </div>
+      <div>
+        <p>Second sermon</p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
