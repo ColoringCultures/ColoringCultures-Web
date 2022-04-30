8c794da7772d16b7c5769c4315eef55b2a8f4cfb
qgatssdev <chivoomodu@gmail.com>
2022-04-30 22:38:27 +0100

    Finishing sub routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Home from './components/root/home';
+import Images from './components/Images/Images';
+import Statistics from './components/Statistics/Statistics';
+import UserFeedback from './components/UserFeedback/UserFeedback';
+import Ads from './components/Ads/Ads';
+import Subscription from './components/Subscription/Subscription';
+import Revenue from './components/Revenue/Revenue';
+import Achievements from './components/Achievements/Achievements';
 
 function App() {
   return (
@@ -10,23 +18,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/Dashboard" element={<Dashboard />}>
-            <Route path="/Dashboard" element={<p>Word</p>} />
-            <Route path="/Dashboard/Images" element={<p>Images</p>} />
-            <Route path="/Dashboard/Statistics" element={<p>Statistics</p>} />
-            <Route
-              path="/Dashboard/Achievements"
-              element={<p>Achievements</p>}
-            />
-            <Route
-              path="/Dashboard/UserFeedback"
-              element={<p>User Feedback</p>}
-            />
-            <Route path="/Dashboard/Ads" element={<p>Ads</p>} />
-            <Route
-              path="/Dashboard/Subscription"
-              element={<p>Subscription</p>}
-            />
-            <Route path="/Dashboard/Revenue" element={<p>Settings</p>} />
+            <Route path="/Dashboard" element={<Home />} />
+            <Route path="/Dashboard/Images" element={<Images />} />
+            <Route path="/Dashboard/Statistics" element={<Statistics />} />
+            <Route path="/Dashboard/Achievements" element={<Achievements />} />
+            <Route path="/Dashboard/UserFeedback" element={<UserFeedback />} />
+            <Route path="/Dashboard/Ads" element={<Ads />} />
+            <Route path="/Dashboard/Subscription" element={<Subscription />} />
+            <Route path="/Dashboard/Revenue" element={<Revenue />} />
           </Route>
         </Routes>
       </Router>
