708b46d353e8110f202d1e3ea86530081bb22890
qgatssdev <chivoomodu@gmail.com>
2022-05-09 21:39:41 +0100

    building userfeedback page and styling dashboard

--- src/components/UserFeedback/UserFeedback.tsx
+++ src/components/UserFeedback/UserFeedback.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Mock } from './mockData';
 
 const UserFeedback = () => {
   return (
-    <div>UserFeedback</div>
-  )
-}
+    <div>
+      <div>UserFeedback</div>
+      <div>
+        {Mock.map((item, index) => {
+          return (
+            <div key={index}>
+              <h1>{item.name}</h1>
+              <p>{item.feedback}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default UserFeedback
+export default UserFeedback;
