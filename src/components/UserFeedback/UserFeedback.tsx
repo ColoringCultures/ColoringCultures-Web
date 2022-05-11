fb8a721f81520a8a8665e6d823ee6bed8aa24ebd
qgatssdev <chivoomodu@gmail.com>
2022-05-11 21:16:42 +0100

    building load more component

--- src/components/UserFeedback/UserFeedback.tsx
+++ src/components/UserFeedback/UserFeedback.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './UserFeedback.scss';
 import { Mock } from './mockData';
 
+const postsPerPage = 6;
+let arrayForHoldingPosts: any = [];
+
 const UserFeedback = () => {
+  const [postsToShow, setPostsToShow] = useState<any[]>([]);
+  const [next, setNext] = useState(6);
+
+  const loopWithSlice = (start: any, end: any) => {
+    const slicedPosts = Mock.slice(start, end);
+    arrayForHoldingPosts = [...arrayForHoldingPosts, ...slicedPosts];
+    setPostsToShow(arrayForHoldingPosts);
+  };
+
+  useEffect(() => {
+    loopWithSlice(0, postsPerPage);
+  }, []);
+
+  const handleShowMorePosts = () => {
+    loopWithSlice(next, next + postsPerPage);
+    setNext(next + postsPerPage);
+  };
   return (
     <div className="feedback-root">
       <div>
         <div>Feedbacks</div>
         <div className="card-root">
-          {Mock.map((item, index) => (
+          {postsToShow.map((item, index) => (
             <div key={index} className="user-border">
               <div className="image-username">
                 <img src={require('../../assets/download (1).png')} alt="" />
@@ -19,6 +39,7 @@ const UserFeedback = () => {
             </div>
           ))}
         </div>
+        <button onClick={handleShowMorePosts}>Load more</button>
       </div>
     </div>
   );
