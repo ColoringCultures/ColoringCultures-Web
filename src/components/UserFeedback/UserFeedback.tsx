bf1b11267e5f8bfef31385be73c2ebdea8e25733
Uche Omodu <chivoomodu@gmail.com>
2022-05-12 08:14:01 -0700

    adding load more functionality

--- src/components/UserFeedback/UserFeedback.tsx
+++ src/components/UserFeedback/UserFeedback.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './UserFeedback.scss';
 import { Mock } from './mockData';
 
+const LENGTH = Mock.length;
+const LIMIT = 6;
 
 const UserFeedback = () => {
-  
+  const [showMore, setShowMore] = useState(true);
+  let listed = Mock.slice(0, LIMIT);
+  const [list, setList] = useState(listed);
+  const [index, setIndex] = useState(LIMIT);
+
+  const loadMore = () => {
+    const newIndex = index + LIMIT;
+    const newShowMore = newIndex < LENGTH - 1;
+    const newList = list.concat(Mock.slice(index, newIndex));
+    setIndex(newIndex);
+    setList(newList);
+    setShowMore(newShowMore);
+  };
   return (
     <div className="feedback-root">
       <div>
         <div>Feedbacks</div>
         <div className="card-root">
-          {Mock.map((item, index) => (
+          {list.map((item, index) => (
             <div key={index} className="user-border">
               <div className="image-username">
                 <img src={require('../../assets/download (1).png')} alt="" />
@@ -21,6 +35,7 @@ const UserFeedback = () => {
             </div>
           ))}
         </div>
+        {showMore && <button onClick={loadMore}> Load More </button>}
       </div>
     </div>
   );
