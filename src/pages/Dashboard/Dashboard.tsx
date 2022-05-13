bfe6ae2ebd78e47794a4877053d0f7bd00906a10
Uche Omodu <chivoomodu@gmail.com>
2022-05-13 17:30:09 +0100

    Styling active images

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Dashboard.scss';
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 
 const Dashboard = () => {
+  const location = useLocation();
+  console.log(location);
+
   return (
     <div className="dashboard-root">
       <div className="navbar-root">
@@ -16,55 +19,105 @@ const Dashboard = () => {
             <h1 className="menu">Menu</h1>
             <div className="links">
               <div className="link">
-                <img src={require('../../assets/Path 2.png')} alt="" />
+                {location.pathname === '/Dashboard/' ? (
+                  <img
+                    src={require('../../assets/active dashboard.png')}
+                    alt=""
+                  />
+                ) : (
+                  <img src={require('../../assets/Path 2.png')} alt="" />
+                )}
                 <NavLink className="navlink" to="/Dashboard/">
                   Dashboard
                 </NavLink>
               </div>
               <div className="link">
-                <img src={require('../../assets/Union 2.png')} alt="" />
+                {location.pathname === '/Dashboard/Images' ? (
+                  <img src={require('../../assets/active images.png')} alt="" />
+                ) : (
+                  <img src={require('../../assets/Union 2.png')} alt="" />
+                )}
                 <NavLink className="navlink" to="/Dashboard/Images">
                   Images
                 </NavLink>
               </div>
               <div className="link">
-                <img src={require('../../assets/Path 19.png')} alt="" />
+                {location.pathname === '/Dashboard/Statistics' ? (
+                  <img
+                    src={require('../../assets/active statistics.png')}
+                    alt=""
+                  />
+                ) : (
+                  <img src={require('../../assets/Path 19.png')} alt="" />
+                )}
                 <NavLink className="navlink" to="/Dashboard/Statistics">
                   Statistics
                 </NavLink>
               </div>
               <div className="link">
-                <img src={require('../../assets/Path 8.png')} alt="" />
+                {location.pathname === '/Dashboard/Achievements' ? (
+                  <img
+                    src={require('../../assets/active achievements.png')}
+                    alt=""
+                  />
+                ) : (
+                  <img src={require('../../assets/Path 8.png')} alt="" />
+                )}
                 <NavLink className="navlink" to="/Dashboard/Achievements">
                   Achievements
                 </NavLink>
               </div>
               <div className="link">
-                <img src={require('../../assets/Path 6.png')} alt="" />
+                {location.pathname === '/Dashboard/UserFeedback' ? (
+                  <img
+                    src={require('../../assets/active Feedback.png')}
+                    alt=""
+                  />
+                ) : (
+                  <img src={require('../../assets/Path 6.png')} alt="" />
+                )}
                 <NavLink className="navlink" to="/Dashboard/UserFeedback">
                   Users Feedback
                 </NavLink>
               </div>
               <div className="link">
-                <img src={require('../../assets/Union 1.png')} alt="" />
+                {location.pathname === '/Dashboard/Ads' ? (
+                  <img src={require('../../assets/active Ads.png')} alt="" />
+                ) : (
+                  <img src={require('../../assets/Union 1.png')} alt="" />
+                )}
                 <NavLink className="navlink" to="/Dashboard/Ads">
                   Ads
                 </NavLink>
               </div>
               <div className="link">
-                <img
-                  src={require('../../assets/Icon feather-package.png')}
-                  alt=""
-                />
+                {location.pathname === '/Dashboard/Subscription' ? (
+                  <img
+                    src={require('../../assets/active subscription.png')}
+                    alt=""
+                  />
+                ) : (
+                  <img
+                    src={require('../../assets/Icon feather-package.png')}
+                    alt=""
+                  />
+                )}
                 <NavLink className="navlink" to="/Dashboard/Subscription">
                   Subscription
                 </NavLink>
               </div>
               <div className="link">
-                <img
-                  src={require('../../assets/Icon feather-package.png')}
-                  alt=""
-                />
+                {location.pathname === '/Dashboard/Revenue' ? (
+                  <img
+                    src={require('../../assets/active subscription.png')}
+                    alt=""
+                  />
+                ) : (
+                  <img
+                    src={require('../../assets/Icon feather-package.png')}
+                    alt=""
+                  />
+                )}
                 <NavLink className="navlink" to="/Dashboard/Revenue">
                   Revenue
                 </NavLink>
