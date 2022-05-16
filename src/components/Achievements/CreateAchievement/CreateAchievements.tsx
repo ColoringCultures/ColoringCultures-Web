1f4a8684901aa1466cddf5ac95c124d01d7d38f2
Uche Omodu <chivoomodu@gmail.com>
2022-05-16 23:13:48 +0100

    building create achievement page

--- src/components/Achievements/CreateAchievement/CreateAchievements.tsx
+++ src/components/Achievements/CreateAchievement/CreateAchievements.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import './CreateAchievement.scss';
 
 const CreateAchievement = () => {
   return (
-    <div>CreateAchievement</div>
-  )
-}
+    <div className="root-create">
+      <div>
+        <p>What Criteria do you want to use?</p>
+        <form>
+          <div className="radio-button">
+            <label htmlFor="color">
+              <input type="radio" value="color" name="criteria" />
+              Color{' '}
+            </label>
+            <label htmlFor="sharing">
+              <input type="radio" value="Sharing" name="criteria" />
+              Sharing
+            </label>{' '}
+            <label htmlFor="pictures">
+              <input type="radio" value="Pictures" name="criteria" />
+              Pictures
+            </label>{' '}
+            <label htmlFor="color$sharing">
+              <input type="radio" value="Color$Sharing" name="criteria" />
+              Color $ Sharing
+            </label>{' '}
+            <label htmlFor="color$sharing">
+              <input type="radio" value="Color$Sharing" name="criteria" />
+              Color $ Sharing
+            </label>{' '}
+            <label htmlFor="color%pictures">
+              <input type="radio" value="Color$Pictures" name="criteria" />
+              Color $ Pictures
+            </label>{' '}
+          </div>
+          <div className="achievement-name">
+            <label> Achievement Name</label>
+            <input type={'text'} placeholder="Enter a name here" />
+          </div>
+          <div className="what-to">
+            <label>What to do:</label>
+            <div className="what-to-sub">
+              Color:
+              <input type={'text'} placeholder="Enter a number" />
+            </div>
+          </div>
+          <button className="create-button">Create Now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default CreateAchievement
+export default CreateAchievement;
