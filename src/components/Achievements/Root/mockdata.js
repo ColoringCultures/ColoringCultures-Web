ed71be853213ffc71a8e618bfb9659b2738580c5
Uche Omodu <chivoomodu@gmail.com>
2022-05-18 14:10:41 +0100

    adding mockdata and image

---
+++ src/components/Achievements/Root/mockdata.js
@@ -0,0 +1,72 @@
+export const mockdata = [
+  {
+    id: '1',
+    title: 'An Artist is born',
+    description: 'You should complete your first image',
+    image: 'smartphone-addiction-line-icon-vector-37660174.png',
+    points: '10',
+  },
+  {
+    id: '2',
+    title: `Don't Quit`,
+    description: 'complete your first image',
+    image: '',
+    points: '16',
+  },
+  {
+    id: '3',
+    title: 'Color and Chill',
+    description: 'complete Fashion images',
+    image:
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    points: '5',
+  },
+  {
+    id: '4',
+    title: 'I want that old thing back',
+    description: 'You should complete your first image',
+    image:
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    points: '60',
+  },
+  {
+    id: '5',
+    title: 'Stay on it',
+    description: 'You should complete your first image',
+    image:
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    points: '10',
+  },
+  {
+    id: '6',
+    title: 'All about location',
+    description: 'You should complete your first image',
+    image:
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    points: '15',
+  },
+  {
+    id: '7',
+    title: 'Did it all',
+    description: 'You should complete your first image',
+    image:
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    points: '10',
+  },
+  {
+    id: '8',
+    title: 'Feeling the Faith',
+    description: 'You should complete your first image',
+    image:
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    points: '70',
+  },
+  {
+    id: '9',
+    title: 'Flawless',
+    description: 'You should complete your first image',
+    image:
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    points: '10',
+  },
+];
