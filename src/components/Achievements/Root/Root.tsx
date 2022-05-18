71ce630dd917c345e120e7db5123711e4b588936
Uche Omodu <chivoomodu@gmail.com>
2022-05-18 14:10:30 +0100

    using mockdata to build UI of achievement page

--- src/components/Achievements/Root/Root.tsx
+++ src/components/Achievements/Root/Root.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { mockdata } from './mockdata';
+import './root.scss';
 
 const Root = () => {
   return (
-    <div>Root</div>
-  )
-}
+    <div className="root-root">
+      {mockdata.map((item, index) => {
+        return (
+          <div className="ach-root" key={index}>
+            <div>
+              <img
+                src={require('../../../assets/smartphone-addiction-line-icon-vector-37660174.png')}
+                alt=""
+              />
+            </div>
+            <div className='ach-root2'>
+              <div className='titleXimage'>
+                <p className="item-title">{item.title}</p>
+              </div>
+              <p className="item-desc">{item.description}</p>
+              <div className='points-root'>
+                <p className="item-points">{item.points}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Root
+export default Root;
