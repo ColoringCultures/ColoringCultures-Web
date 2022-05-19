017b519248061c225133d5d5a6d2e69f403ca1a7
Uche Omodu <chivoomodu@gmail.com>
2022-05-19 11:40:27 +0100

    Styling the ads nav bar

--- src/components/Ads/Ads.tsx
+++ src/components/Ads/Ads.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavLink from '../../Navlink';
+import './Ads.scss';
 
 const Ads = () => {
   return (
-    <div>Ads</div>
-  )
-}
+    <div>
+      <div className="root-Ads">
+        <h1>Adss</h1>
+        <div className="Ads-header">
+          <NavLink
+            className="Ads-link"
+            to="/Dashboard/Ads"
+            exact={true}
+            activeClassName="active"
+            inactiveClassName="Ads-Link"
+          >
+            All
+          </NavLink>
+          <NavLink
+            className="Ads-link"
+            to="AddAds"
+            exact={false}
+            activeClassName="active"
+            inactiveClassName="Ads-Link"
+          >
+            + Add Ads
+          </NavLink>
+          <NavLink
+            className="Ads-link"
+            to="EditAds"
+            exact={false}
+            activeClassName="active"
+            inactiveClassName="Ads-Link"
+          >
+            Edit Ads
+          </NavLink>
+        </div>
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Ads
+export default Ads;
