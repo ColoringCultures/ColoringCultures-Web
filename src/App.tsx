dcbe57f0b839a60a373305a2d8e2274ea1994526
Uche Omodu <chivoomodu@gmail.com>
2022-05-23 23:48:07 +0100

    refactoring router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
+import { useState } from 'react';
+import { UserContext } from './UserContext';
 
 function App() {
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState('');
+
   return (
     <Router>
-      <Routes />
+      <UserContext.Provider value={{ token, setToken, user, setUser }}>
+        <Routes />
+      </UserContext.Provider>
     </Router>
   );
 }
