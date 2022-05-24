646d64c17d0f72ca6ddfb4b00965007e7fdd16b3
Uche Omodu <chivoomodu@gmail.com>
2022-05-24 15:17:57 +0100

    testing of token

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const Statistics = () => {
-  return (
-    <div>Statistics</div>
-  )
-}
+  return <div>Statistics</div>;
+};
 
-export default Statistics
+export default Statistics;
