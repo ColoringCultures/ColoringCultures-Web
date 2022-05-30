7a33c7d6f12f234b01ca31a46b118867b192a4dc
Uche Omodu <chivoomodu@gmail.com>
2022-05-30 17:58:20 +0100

    adding infinte scroll pagination to acheivement page

--- src/components/Achievements/Root/Root.tsx
+++ src/components/Achievements/Root/Root.tsx
@@ -1,32 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { mockdata } from './mockdata';
 import './root.scss';
 
+const LENGTH = mockdata.length;
+const LIMIT = 6;
+
 const Root = () => {
+  const [showMore, setShowMore] = useState(true);
+  let listed = mockdata.slice(0, LIMIT);
+  const [list, setList] = useState(listed);
+  const [index, setIndex] = useState(LIMIT);
+
+  const loadMore = () => {
+    const newIndex = index + LIMIT;
+    const newShowMore = newIndex < LENGTH - 1;
+    const newList = list.concat(mockdata.slice(index, newIndex));
+    setIndex(newIndex);
+    setList(newList);
+    setShowMore(newShowMore);
+  };
   return (
-    <div className="root-root">
-      {mockdata.map((item, index) => {
-        return (
-          <div className="ach-root" key={index}>
-            <div>
-              <img
-                src={require('../../../assets/smartphone-addiction-line-icon-vector-37660174.png')}
-                alt=""
-              />
-            </div>
-            <div className='ach-root2'>
-              <div className='titleXimage'>
-                <p className="item-title">{item.title}</p>
+    <>
+      <div className="root-root">
+        {list.map((item, index) => {
+          return (
+            <div className="ach-root" key={index}>
+              <div>
+                <img
+                  src={require('../../../assets/smartphone-addiction-line-icon-vector-37660174.png')}
+                  alt=""
+                />
               </div>
-              <p className="item-desc">{item.description}</p>
-              <div className='points-root'>
-                <p className="item-points">{item.points}</p>
+              <div className="ach-root2">
+                <div className="titleXimage">
+                  <p className="item-title">{item.title}</p>
+                  <img
+                    src={require('../../../assets/207-2071219_arrow-share-conversion-comments-share-icon.png')}
+                    alt=""
+                  />
+                </div>
+                <p className="item-desc">{item.description}</p>
+                <div className="points-root">
+                  <p className="item-points">{item.points}</p>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      {showMore && (
+        <button onClick={loadMore} className="achievement-button ">
+          {' '}
+          Load More{' '}
+        </button>
+      )}
+    </>
   );
 };
 
