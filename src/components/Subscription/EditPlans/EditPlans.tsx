dc641e7729865e7fd7e69af255cd4701f33a49c8
Uche Omodu <chivoomodu@gmail.com>
2022-05-31 13:47:22 +0100

    adding edit modal

--- src/components/Subscription/EditPlans/EditPlans.tsx
+++ src/components/Subscription/EditPlans/EditPlans.tsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './EditPlans.scss';
+import Modal from '../Modal/Modal';
 
 const EditPlans = () => {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-    <div>EditPlans</div>
-  )
-}
+    <div>
+      <div className="editplan-root">
+        <div>
+          <div>
+            <label>Plan Name</label>
+            <input type="text" placeholder="Eg. basic" />
+          </div>
+          <div className="radio-plan">
+            <label>
+              Art to be coloured
+              <div className="radio-radio">
+                <input
+                  type="checkbox"
+                  value="color"
+                  name="art"
+                  className="checkbox"
+                />{' '}
+                Unlimited
+              </div>
+            </label>
+            <input type="text" placeholder="200" />
+          </div>
+          <div className="radio-plan">
+            <label>
+              Number of Arts
+              <div className="radio-radio">
+                <input
+                  type="checkbox"
+                  value="color"
+                  name="art"
+                  className="checkbox"
+                />{' '}
+                Unlimited
+              </div>
+            </label>
+            <input type="text" placeholder="Enter number" />
+          </div>
+          <div className="radio-plan">
+            <label>Amount</label>
+            <input type="text" placeholder="Enter number" />
+          </div>
+        </div>
+        <div className="left-div-plan">
+          <div className="radio-plan">
+            <label>
+              Amount of hint
+              <div className="radio-radio">
+                <input
+                  type="checkbox"
+                  value="color"
+                  name="art"
+                  className="checkbox"
+                />{' '}
+                Unlimited
+              </div>
+            </label>
+            <input type="text" placeholder="Amount of hints" />
+          </div>
+          <div className="upload-plans">
+            <label>
+              Choose the plan avatar
+              <label className="plan-label">
+                Choose a files
+                <input type={'file'} placeholder="Choose a file" />
+              </label>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="ad-buttons">
+        <button className="create-ad-button">Create Ad</button>
+        <button
+          className="delete-ad-button"
+          onClick={() => {
+            setModalOpen(true);
+          }}
+        >
+          Delete Ad
+        </button>
+      </div>
+      {modalOpen && <Modal setOpenModal={setModalOpen} Modal={modalOpen} />}
+    </div>
+  );
+};
 
-export default EditPlans
+export default EditPlans;
