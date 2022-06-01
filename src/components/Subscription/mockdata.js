cd82e41b4ed2f6e7f30bae4c02a6cd0edcd38732
Uche Omodu <chivoomodu@gmail.com>
2022-06-01 20:41:45 +0100

    adjusting mock data for sub

--- src/components/Subscription/mockdata.js
+++ src/components/Subscription/mockdata.js
@@ -23,4 +23,28 @@ export const Mock = [
     amount: '10',
     amountOfHint: '10',
   },
+  {
+    id: '1',
+    name: 'Standard',
+    artToBeColored: '200',
+    numOfArt: '200',
+    amount: '10',
+    amountOfHint: '10',
+  },
+  {
+    id: '2',
+    name: 'Medium',
+    artToBeColored: '200',
+    numOfArt: '200',
+    amount: '10',
+    amountOfHint: '10',
+  },
+  {
+    id: '3',
+    name: 'Pro',
+    artToBeColored: '200',
+    numOfArt: '200',
+    amount: '10',
+    amountOfHint: '10',
+  },
 ];
