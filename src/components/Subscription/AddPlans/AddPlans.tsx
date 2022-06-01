21a8f37b25a6434b4f48305da125cbed0327dcd0
Uche Omodu <79480742+qgatssdev@users.noreply.github.com>
2022-06-01 20:14:54 +0100

    Development (#16)
    * adding modal logic to delete button
    
    * Adding Modal to ads page
    
    * adding Modals to Achievement page
    
    * stats
    
    * fixing bug in modal
    
    * adding modal to achievement page
    
    * creating signout Modal
    
    * adding signout modal to signout button
    
    * fixed login routing
    
    * adding infinite scroll to feedback
    
    * adjusting height
    
    * setting specific height for div
    
    * image for acheivements edit
    
    * restyling active state of links
    
    * adding infinte scroll pagination to acheivement page
    
    * icon file
    
    * typo fix
    
    * adding logo
    
    * adding min and max width to navbar
    
    * adjusting the login logo and position
    
    * adding error message and typo fixes
    
    * adding edit modal
    
    * delete modal
    
    * adding icons
    
    * adding mockdata for sub page
    
    * styling root sub plans
    
    * hook form

--- src/components/Subscription/AddPlans/AddPlans.tsx
+++ src/components/Subscription/AddPlans/AddPlans.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddPlans.scss';
 
 const AddPlans = () => {
+  function imageUpload(event: any) {
+    const image = event.target.files[0];
+    setPlanAvatar(image);
+  }
+
+  const createSubscription = () => {
+    console.log(
+      planName,
+      numOfArts,
+      artToBeColored,
+      planAvatar,
+      numOfHint,
+      amount
+    );
+  };
+
+  const [planName, setPlanName] = useState('');
+  const [artToBeColored, setArtToBeColored] = useState('200');
+  const [numOfArts, setNumOfArts] = useState('');
+  const [numOfHint, setNumOfHint] = useState('');
+  const [amount, setAmount] = useState('');
+  const [planAvatar, setPlanAvatar] = useState();
+  const [unlimtedColors, setUnlimtedColors] = useState(false);
+  const [unlimtedArts, setUnlimtedArts] = useState(false);
+  const [unlimtedHints, setUnlimtedHints] = useState(false);
+  console.log(numOfArts);
+
   return (
     <div>
       <div className="addplan-root">
         <div>
           <div>
             <label>Plan Name</label>
-            <input type="text" placeholder="Eg. basic" />
+            <input
+              type="text"
+              placeholder="Eg. basic"
+              onChange={(e) => {
+                setPlanName(e.target.value);
+              }}
+            />
           </div>
+
           <div className="radio-plan">
             <label>
               Art to be coloured
@@ -19,12 +53,25 @@ const AddPlans = () => {
                   value="color"
                   name="art"
                   className="checkbox"
+                  onClick={() => {
+                    setUnlimtedColors(!unlimtedColors);
+                  }}
                 />{' '}
                 Unlimited
               </div>
             </label>
-            <input type="text" placeholder="200" />
+
+            <input
+              type="text"
+              placeholder={unlimtedColors ? 'Unlimited' : 'Eg. 200'}
+              onChange={(e) => {
+                unlimtedColors
+                  ? setArtToBeColored('Unlimited')
+                  : setArtToBeColored(e.target.value);
+              }}
+            />
           </div>
+
           <div className="radio-plan">
             <label>
               Number of Arts
@@ -34,18 +81,38 @@ const AddPlans = () => {
                   value="color"
                   name="art"
                   className="checkbox"
+                  onClick={() => {
+                    setUnlimtedArts(!unlimtedArts);
+                  }}
                 />{' '}
                 Unlimited
               </div>
             </label>
-            <input type="text" placeholder="Enter number" />
+
+            <input
+              type="text"
+              placeholder={unlimtedArts ? 'Unlimited' : 'Eg. 200'}
+              onChange={(e) => {
+                unlimtedArts
+                  ? setNumOfArts('Unlimited')
+                  : setNumOfArts(e.target.value);
+              }}
+            />
           </div>
+
           <div className="radio-plan">
             <label>Amount</label>
-            <input type="text" placeholder="Enter number" />
+            <input
+              type="text"
+              placeholder="Enter number"
+              onChange={(e) => {
+                setAmount(e.target.value);
+              }}
+            />
           </div>
         </div>
-        <div className='left-div-plan'>
+
+        <div className="left-div-plan">
           <div className="radio-plan">
             <label>
               Amount of hint
@@ -55,24 +122,43 @@ const AddPlans = () => {
                   value="color"
                   name="art"
                   className="checkbox"
+                  onClick={() => {
+                    setUnlimtedHints(!unlimtedHints);
+                  }}
                 />{' '}
                 Unlimited
               </div>
             </label>
-            <input type="text" placeholder="Amount of hints" />
+
+            <input
+              type="text"
+              placeholder={unlimtedHints ? 'Unlimited' : 'Eg. 400'}
+              onChange={(e) => {
+                unlimtedHints
+                  ? setNumOfHint('Unlimited')
+                  : setNumOfHint(e.target.value);
+              }}
+            />
           </div>
+
           <div className="upload-plans">
             <label>
               Choose the plan avatar
               <label className="plan-label">
                 Choose a files
-                <input type={'file'} placeholder="Choose a file" />
+                <input
+                  type={'file'}
+                  placeholder="Choose a file"
+                  onChange={imageUpload}
+                />
               </label>
             </label>
           </div>
         </div>
       </div>
-      <button className="plan-button">Create Ad</button>
+      <button className="plan-button" onClick={createSubscription}>
+        Create Ad
+      </button>
     </div>
   );
 };
--- src/components/Subscription/root/Plans.tsx
+++ src/components/Subscription/root/Plans.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import './Plans.scss';
+import { Mock } from '../mockdata';
 
 const Plans = () => {
   return (
-    <div>Plans</div>
-  )
-}
+    <div className="sub-root">
+      {Mock.map((data, index) => {
+        return (
+          <div key={index} className="plan-root">
+            <div className="plan-header">
+              <div>
+                <img
+                  src={require('../../../assets/Rectangle 121.png')}
+                  alt=""
+                />
+              </div>
+              <div className="plan-header-details">
+                <div className="details-name">{data.name}</div>
+                <div className="details-amount">
+                  ${data.amount}
+                  <span>/month</span>
+                </div>
+              </div>
+            </div>
+            <div className="details-root">
+              <div className="flex-details">
+                <img
+                  src={require('../../../assets/Icon ionic-md-checkmark.png')}
+                  alt=""
+                />
+                <p>Color up to {data.artToBeColored}</p>
+              </div>
+              <div className="flex-details">
+                <img
+                  src={require('../../../assets/Icon ionic-md-checkmark.png')}
+                  alt=""
+                />
+                <p>{data.amountOfHint} free hint</p>
+              </div>
+              <div className="flex-details">
+                <img
+                  src={require('../../../assets/Icon ionic-md-checkmark.png')}
+                  alt=""
+                />
+                <p>No ads</p>
+              </div>
+              <div className="flex-details">
+                <img
+                  src={require('../../../assets/Icon ionic-md-checkmark.png')}
+                  alt=""
+                />
+                <p>Color pro Arts</p>
+              </div>
+            </div>
+            <div className="button-plans">
+              <button>Edit</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Plans
+export default Plans;
