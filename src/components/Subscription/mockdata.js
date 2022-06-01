bf01220082bf708a046624bbff0e47018e79ac5c
Uche Omodu <chivoomodu@gmail.com>
2022-06-01 19:51:02 +0100

    adding mockdata for sub page

---
+++ src/components/Subscription/mockdata.js
@@ -0,0 +1,26 @@
+export const Mock = [
+  {
+    id: '1',
+    name: 'Standard',
+    artToBeColored: '200',
+    numOfArt: '200',
+    amount: '10',
+    amountOfHint: '10',
+  },
+  {
+    id: '2',
+    name: 'Medium',
+    artToBeColored: '200',
+    numOfArt: '200',
+    amount: '10',
+    amountOfHint: '10',
+  },
+  {
+    id: '3',
+    name: 'Pro',
+    artToBeColored: '200',
+    numOfArt: '200',
+    amount: '10',
+    amountOfHint: '10',
+  },
+];
