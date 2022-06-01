757242233a86916da8230ac0af0ece5bcf8219eb
Uche Omodu <chivoomodu@gmail.com>
2022-06-01 22:57:05 +0100

    Building Suggestion page

--- src/components/UserFeedback/Suggestions/Suggestions.tsx
+++ src/components/UserFeedback/Suggestions/Suggestions.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Suggestions.scss';
+import { Mock } from '../mockData';
+
+const LENGTH = Mock.length;
+const LIMIT = 4;
 
 const Suggestions = () => {
-  return (
-    <div>Suggestions</div>
-  )
-}
+  const [showMore, setShowMore] = useState(true);
+  let listed = Mock.slice(0, LIMIT);
+  const [list, setList] = useState(listed);
+  const [index, setIndex] = useState(LIMIT);
 
-export default Suggestions
+  const loadMore = () => {
+    const newIndex = index + LIMIT;
+    const newShowMore = newIndex < LENGTH - 1;
+    const newList = list.concat(Mock.slice(index, newIndex));
+    setIndex(newIndex);
+    setList(newList);
+    setShowMore(newShowMore);
+  };
+  return (
+    <div className="feed-root">
+      <div className="card-root">
+        {list.map((item, index) => (
+          <div key={index} className="user-border">
+            <div className="image-username">
+              <img src={require('../../../assets/download (1).png')} alt="" />
+              <h1>{item.name}</h1>
+            </div>
+            <p>{item.feedback}</p>
+          </div>
+        ))}
+      </div>
+      {showMore && (
+        <button onClick={loadMore} className="submit-button">
+          {' '}
+          Load More{' '}
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Suggestions;
