4ca29145b1be43ff4dc100a15e23739bb2cf217b
Uche Omodu <chivoomodu@gmail.com>
2022-06-01 22:37:15 +0100

    Creating BugReport, Feature Requests, Praise and Suggestions page

---
+++ src/components/UserFeedback/BugReport/BugReport.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './BugReport.scss';
+import { Mock } from '../mockData';
+
+const LENGTH = Mock.length;
+const LIMIT = 4;
+
+const BugReport = () => {
+  const [showMore, setShowMore] = useState(true);
+  let listed = Mock.slice(0, LIMIT);
+  const [list, setList] = useState(listed);
+  const [index, setIndex] = useState(LIMIT);
+
+  const loadMore = () => {
+    const newIndex = index + LIMIT;
+    const newShowMore = newIndex < LENGTH - 1;
+    const newList = list.concat(Mock.slice(index, newIndex));
+    setIndex(newIndex);
+    setList(newList);
+    setShowMore(newShowMore);
+  };
+  return (
+    <div className="feed-root">
+      <div className="card-root">
+        {list.map((item, index) => (
+          <div key={index} className="user-border">
+            <div className="image-username">
+              <img src={require('../../../assets/download (1).png')} alt="" />
+              <h1>{item.name}</h1>
+            </div>
+            <p>{item.feedback}</p>
+          </div>
+        ))}
+      </div>
+      {showMore && (
+        <button onClick={loadMore} className="submit-button">
+          {' '}
+          Load More{' '}
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default BugReport;
---
+++ src/components/UserFeedback/Praise/Praise.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Praise = () => {
+  return (
+    <div>Praise</div>
+  )
+}
+
+export default Praise
