a666262d2f5e072baea8bf8f9595f395e0fa5335
Uche Omodu <chivoomodu@gmail.com>
2022-06-02 22:43:05 +0100

    yup schema for validation

---
+++ src/components/Ads/schema.tsx
@@ -0,0 +1,19 @@
+import * as yup from 'yup';
+
+export const schema = yup.object().shape({
+  title: yup.string().required('Title is required'),
+  redirect_url: yup.string().required('Url is required'),
+  time_feed: yup
+    .number()
+    .positive()
+    .integer()
+    .typeError('Watch time must be a number')
+    .required('Watch Time is required'),
+  ad_target: yup
+    .number()
+    .positive()
+    .integer()
+    .typeError('Target must be a number')
+    .required('Target is required'),
+  file: yup.mixed().required(),
+});
