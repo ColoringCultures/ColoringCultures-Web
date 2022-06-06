8dc65684a861f194e1f5135d966f80c95b344d55
Uche Omodu <chivoomodu@gmail.com>
2022-06-06 16:19:54 +0100

    typo fix

--- src/components/UserFeedback/BugReport/BugReport.tsx
+++ src/components/UserFeedback/BugReport/BugReport.tsx
@@ -22,7 +22,7 @@ const BugReport = () => {
     setScroll(true);
   };
   return (
-    <div className="feed-root">
+    <div>
       <div className={scroll ? 'card-root' : 'scroll-card-root'}>
         {list.map((item, index) => (
           <div key={index} className="user-border">
