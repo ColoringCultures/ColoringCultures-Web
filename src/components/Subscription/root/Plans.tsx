7ffd30898e814c909b16aed3351e00b54f0d667f
Uche Omodu <chivoomodu@gmail.com>
2022-06-07 11:22:13 +0100

    adjusting width of containers

--- src/components/Subscription/root/Plans.tsx
+++ src/components/Subscription/root/Plans.tsx
@@ -22,61 +22,63 @@ const Plans = () => {
     setScroll(true);
   };
   return (
-    <div className={scroll ? "sub-root" : "scroll-sub-root"}>
-      {list.map((data, index) => {
-        return (
-          <div key={index} className="plan-root">
-            <div className="plan-header">
-              <div>
-                <img
-                  src={require('../../../assets/Rectangle 121.png')}
-                  alt=""
-                />
-              </div>
-              <div className="plan-header-details">
-                <div className="details-name">{data.name}</div>
-                <div className="details-amount">
-                  ${data.amount}
-                  <span>/month</span>
+    <div>
+      <div className={scroll ? 'sub-root' : 'scroll-sub-root'}>
+        {list.map((data, index) => {
+          return (
+            <div key={index} className="plan-root">
+              <div className="plan-header">
+                <div>
+                  <img
+                    src={require('../../../assets/Rectangle 121.png')}
+                    alt=""
+                  />
+                </div>
+                <div className="plan-header-details">
+                  <div className="details-name">{data.name}</div>
+                  <div className="details-amount">
+                    ${data.amount}
+                    <span>/month</span>
+                  </div>
                 </div>
               </div>
+              <div className="details-root">
+                <div className="flex-details">
+                  <img
+                    src={require('../../../assets/Icon ionic-md-checkmark.png')}
+                    alt=""
+                  />
+                  <p>Color up to {data.artToBeColored}</p>
+                </div>
+                <div className="flex-details">
+                  <img
+                    src={require('../../../assets/Icon ionic-md-checkmark.png')}
+                    alt=""
+                  />
+                  <p>{data.amountOfHint} free hint</p>
+                </div>
+                <div className="flex-details">
+                  <img
+                    src={require('../../../assets/Icon ionic-md-checkmark.png')}
+                    alt=""
+                  />
+                  <p>No ads</p>
+                </div>
+                <div className="flex-details">
+                  <img
+                    src={require('../../../assets/Icon ionic-md-checkmark.png')}
+                    alt=""
+                  />
+                  <p>Color pro Arts</p>
+                </div>
+              </div>
+              <div className="button-plans">
+                <button>Edit</button>
+              </div>
             </div>
-            <div className="details-root">
-              <div className="flex-details">
-                <img
-                  src={require('../../../assets/Icon ionic-md-checkmark.png')}
-                  alt=""
-                />
-                <p>Color up to {data.artToBeColored}</p>
-              </div>
-              <div className="flex-details">
-                <img
-                  src={require('../../../assets/Icon ionic-md-checkmark.png')}
-                  alt=""
-                />
-                <p>{data.amountOfHint} free hint</p>
-              </div>
-              <div className="flex-details">
-                <img
-                  src={require('../../../assets/Icon ionic-md-checkmark.png')}
-                  alt=""
-                />
-                <p>No ads</p>
-              </div>
-              <div className="flex-details">
-                <img
-                  src={require('../../../assets/Icon ionic-md-checkmark.png')}
-                  alt=""
-                />
-                <p>Color pro Arts</p>
-              </div>
-            </div>
-            <div className="button-plans">
-              <button>Edit</button>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       {showMore && (
         <button onClick={loadMore} className="sub-loadmore-button">
           {' '}
