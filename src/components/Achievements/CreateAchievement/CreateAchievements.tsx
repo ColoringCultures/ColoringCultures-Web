b4d7a5c2f42a683003ae603f7dd40740f2e1cf35
Uche Omodu <chivoomodu@gmail.com>
2022-06-09 15:20:12 +0100

    building achievement root

--- src/components/Achievements/CreateAchievement/CreateAchievements.tsx
+++ src/components/Achievements/CreateAchievement/CreateAchievements.tsx
@@ -1,52 +1,45 @@
-import React from 'react';
 import './CreateAchievement.scss';
-//
+
 const CreateAchievement = () => {
   return (
     <div className="root-create">
-      <div>
-        <p>What Criteria do you want to use?</p>
-        <form>
-          <div className="radio-button">
-            <label htmlFor="color">
-              <input type="radio" value="color" name="criteria" />
-              Color{' '}
-            </label>
-            <label htmlFor="sharing">
-              <input type="radio" value="Sharing" name="criteria" />
-              Sharing
-            </label>{' '}
-            <label htmlFor="pictures">
-              <input type="radio" value="Pictures" name="criteria" />
-              Pictures
-            </label>{' '}
-            <label htmlFor="color$sharing">
-              <input type="radio" value="Color$Sharing" name="criteria" />
-              Color $ Sharing
-            </label>{' '}
-            <label htmlFor="color$sharing">
-              <input type="radio" value="Color$Sharing" name="criteria" />
-              Color $ Sharing
-            </label>{' '}
-            <label htmlFor="color%pictures">
-              <input type="radio" value="Color$Pictures" name="criteria" />
-              Color $ Pictures
-            </label>{' '}
-          </div>
-          <div className="achievement-name">
-            <label> Achievement Name</label>
-            <input type={'text'} placeholder="Enter a name here" />
-          </div>
-          <div className="what-to">
-            <label>What to do:</label>
-            <div className="what-to-sub">
-              Color:
-              <input type={'text'} placeholder="Enter a number" />
+      <form>
+        <div className="create-root">
+          <div className="create-root-div1">
+            <div className="create-input-text">
+              <label htmlFor="Achievement Name">Achievement Name</label>
+              <input type="text" placeholder="Enter a number here" />
+            </div>
+            <div className="create-input-text">
+              <label htmlFor="Description">Description</label>
+              <input type="text" placeholder="Enter text" />
+            </div>
+            <div className="create-input-text-criteria">
+              <label htmlFor="Criteria">Criteria</label>
+              <input type="text" placeholder="Enter a number" />
             </div>
           </div>
-          <button className="create-button">Create Now</button>
-        </form>
-      </div>
+          <div className="create-root-div2">
+            <div className='file-root'>
+              <label htmlFor=""> Upload initial and final images. (SVG)</label>
+              <div>
+                <div>
+                  <label>Dark-mode</label>
+                  <input type="file" />
+                </div>
+                <div>
+                  <label>Light-mode</label>
+                  <input type="file" />
+                </div>
+                <div>
+                  <label>Colored</label>
+                  <input type="file" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
