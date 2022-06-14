630d92e27f5377be0aec344a02ffb0691a4afb53
Uche Omodu <chivoomodu@gmail.com>
2022-06-14 10:06:01 +0100

    Changes to reset function

--- src/components/Ads/AddAds/AddAds.tsx
+++ src/components/Ads/AddAds/AddAds.tsx
@@ -51,13 +51,10 @@ const AddAds = () => {
     if (response.data.message === 'OK') {
       setModalOpen(true);
     }
-  };
-
-  useEffect(() => {
     reset({
       data: '',
     });
-  });
+  };
 
   useEffect(() => {
     if (modalOpen) {
