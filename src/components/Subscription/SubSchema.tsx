0f65d8439b6ebc634f5c8e09c4323afb437e31e6
Uche Omodu <chivoomodu@gmail.com>
2022-06-14 20:36:59 +0100

    deleting subscheme

---
+++ src/components/Subscription/SubSchema.tsx
@@ -0,0 +1,36 @@
+import * as yup from 'yup';
+
+export const schema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  task: yup.string().required('Description is required'),
+  criteria: yup
+    .number()
+    .positive()
+    .integer()
+    .typeError('Criteria must be a number')
+    .required('Criteria is required'),
+  icon_image: yup
+    .mixed()
+    .test('required', 'Provide a file', (value) => {
+      return value && value.length;
+    })
+    .test('fileSize', 'The file is too large', (value) => {
+      return value && value[0] && value[0].size <= 200000;
+    }),
+  colored_icon_image: yup
+    .mixed()
+    .test('required', 'Provide a file', (value) => {
+      return value && value.length;
+    })
+    .test('fileSize', 'The file is too large', (value) => {
+      return value && value[0] && value[0].size <= 200000;
+    }),
+  dark_icon_image: yup
+    .mixed()
+    .test('required', 'Provide a file', (value) => {
+      return value && value.length;
+    })
+    .test('fileSize', 'The file is too large', (value) => {
+      return value && value[0] && value[0].size <= 200000;
+    }),
+});
