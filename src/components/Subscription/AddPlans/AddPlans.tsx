0ed6d92d809d4ba5e00ef6dabd95f9fb3ad29355
Uche Omodu <79480742+qgatssdev@users.noreply.github.com>
2022-06-17 00:08:38 +0100

    Development (#38)
    * building achievement root
    
    * editing ads page
    
    * Building Create page
    
    * typo fix
    
    * adding create achivement styles
    
    * Styling Edit
    
    * advertisement schema
    
    * testing changes
    
    * Creating achivement schema
    
    * commenting out scss
    
    * Making changes to ads schema
    
    * Building Create page
    
    * Creating Loader component
    
    * Modal for Create Achievement
    
    * File schema
    
    * Finishing up Create page
    
    * Finshing Ads page
    
    * adding hover for buttons
    
    * adding hover for buttons
    
    * hover for button
    
    * Building Feedback page with endpoint
    
    * adding hover to button
    
    * changes to reset function
    
    * chnages to min width
    
    * changes to height of container
    
    * Changes to reset function
    
    * deleting subscheme
    
    * Finishing edit plans
    
    * Finishing Ad plans
    
    * styling change
    
    * deleting schema
    
    * typo error in component
    
    * Building subpage with api integratoin
    
    * building
    
    * updating loadmore function
    
    * fixing bug
    
    * updates
    
    * fixing bugs
    
    * updating styling
    
    * Update AddPlans.tsx
    
    * Modal for create achievement
    
    * finishing up

--- src/components/Subscription/AddPlans/AddPlans.tsx
+++ src/components/Subscription/AddPlans/AddPlans.tsx
@@ -2,16 +2,26 @@ import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import './AddPlans.scss';
 import { UserContext } from '../../../UserContext';
+import Loader from '../../../Loader/Loader';
+import Modal from './Modal/Modal';
+
 
 const AddPlans = () => {
   const { token } = useContext(UserContext);
   const formData = new FormData();
+
+  const [isLoading, setLoading] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  // const [errMsg, setErrMsg] = useState('');
 
   function imageUpload(event: any) {
     formData.append('plan_avatar', event.target.files[0]);
   }
 
   const createSubscription = async () => {
+
+    setLoading(true);
     formData.append('plan_name', planName);
     formData.append('art_to_be_colored', artToBeColored);
     formData.append('number_of_arts', numOfArts);
@@ -27,6 +37,10 @@ const AddPlans = () => {
         },
       }
     );
+    setLoading(false);
+    if (response.data.message === 'OK') {
+      setModalOpen(true);
+    }
     console.log(response);
   };
 
@@ -44,6 +58,14 @@ const AddPlans = () => {
   const [unlimtedArts, setUnlimtedArts] = useState(false);
   const [unlimtedHints, setUnlimtedHints] = useState(false);
   const [image, setImage] = useState('');
+
+  useEffect(() => {
+    if (modalOpen) {
+      setTimeout(() => {
+        setModalOpen(false);
+      }, 2000);
+    }
+  });
 
   useEffect(() => {
     if (unlimtedHints) {
@@ -65,138 +87,148 @@ const AddPlans = () => {
 
   return (
     <div>
-      <form action="">
-        <div className="addplan-root">
-          <div className="right-ads-div">
-            <div>
-              <label>Plan Name</label>
-              <input
-                type="text"
-                placeholder="Eg. basic"
-                onChange={(e) => {
-                  setPlanName(e.target.value);
-                }}
-                required
-              />
-            </div>
-            <div className="radio-plan">
-              <div className="label">
-                <label>Art to be coloured</label>
-                <div className="radio-radio">
-                  <input
-                    type="checkbox"
-                    value="color"
-                    name="art"
-                    className="checkbox"
-                    onClick={() => {
-                      setUnlimtedColors(!unlimtedColors);
-                    }}
-                  />{' '}
-                  Unlimited
-                </div>
-              </div>
-              <input
-                type="text"
-                placeholder={unlimtedColors ? 'Unlimited' : 'Eg. 200'}
-                onChange={(e) => {
-                  setArtToBeColored(e.target.value);
-                }}
-                disabled={unlimtedColors}
-                required
-              />
-            </div>
-            <div className="radio-plan">
-              <div className="label">
-                <label>Number of Arts</label>
-                <div className="radio-radio">
-                  <input
-                    type="checkbox"
-                    value="color"
-                    name="art"
-                    className="checkbox"
-                    onClick={() => {
-                      setUnlimtedArts(!unlimtedArts);
-                    }}
-                  />{' '}
-                  Unlimited
-                </div>
-              </div>
-              <input
-                type="text"
-                placeholder={unlimtedArts ? 'Unlimited' : 'Eg. 200'}
-                onChange={(e) => {
-                  setNumOfArts(e.target.value);
-                }}
-                disabled={unlimtedArts}
-                required
-              />
-            </div>
-            <div className="radio-plan">
-              <label>Amount</label>
-              <input
-                type="text"
-                placeholder="Enter number"
-                onChange={(e) => {
-                  setAmount(e.target.value);
-                }}
-              />
-            </div>
-          </div>
-          <div className="left-div-plan">
-            <div className="radio-plan">
-              <div className="label">
-                <label>Amount of hint</label>
-                <div className="radio-radio">
-                  <input
-                    type="checkbox"
-                    value="color"
-                    name="art"
-                    className="checkbox"
-                    onClick={() => {
-                      setUnlimtedHints(!unlimtedHints);
-                    }}
-                  />{' '}
-                  Unlimited
-                </div>
-              </div>
-              <input
-                type="text"
-                placeholder={unlimtedHints ? 'Unlimited' : 'Eg. 400'}
-                onChange={(e) => {
-                  setNumOfHint(e.target.value);
-                }}
-                disabled={unlimtedHints}
-                required
-              />
-            </div>
-            <div className="upload-plans">
-              <label>
-                Choose the plan avatar
-                <label className="plan-label">
-                  Choose a files
-                  <input
-                    type={'file'}
-                    placeholder="Choose a file"
-                    onInput={displayImage}
-                    onChange={imageUpload}
-
-                  />
-                </label>
-              </label>
-              {image && (
-                <img
-                  style={{ width: '241px', height: ' 270px' }}
-                  src={image}
-                  alt=""
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <form>
+          <div className="addplan-root">
+            <div className="right-ads-div">
+              <div>
+                <label>Plan Name</label>
+                <input
+                  type="text"
+                  placeholder="Eg. basic"
+                  onChange={(e) => {
+                    setPlanName(e.target.value);
+                  }}
+                  required
                 />
-              )}
+              </div>
+              <div className="radio-plan">
+                <div className="label">
+                  <label>Art to be coloured</label>
+                  <div className="radio-radio">
+                    <input
+                      type="checkbox"
+                      value="color"
+                      name="art"
+                      className="checkbox"
+                      onClick={() => {
+                        setUnlimtedColors(!unlimtedColors);
+                      }}
+                    />{' '}
+                    Unlimited
+                  </div>
+                </div>
+                <input
+                  type="text"
+                  placeholder={unlimtedColors ? 'Unlimited' : 'Eg. 200'}
+                  onChange={(e) => {
+                    setArtToBeColored(e.target.value);
+                  }}
+                  disabled={unlimtedColors}
+                  required
+                />
+              </div>
+              <div className="radio-plan">
+                <div className="label">
+                  <label>Number of Arts</label>
+                  <div className="radio-radio">
+                    <input
+                      type="checkbox"
+                      value="color"
+                      name="art"
+                      className="checkbox"
+                      onClick={() => {
+                        setUnlimtedArts(!unlimtedArts);
+                      }}
+                    />{' '}
+                    Unlimited
+                  </div>
+                </div>
+                <input
+                  type="text"
+                  placeholder={unlimtedArts ? 'Unlimited' : 'Eg. 200'}
+                  onChange={(e) => {
+                    setNumOfArts(e.target.value);
+                  }}
+                  disabled={unlimtedArts}
+                  required
+                />
+              </div>
+              <div className="radio-plan">
+                <label>Amount</label>
+                <input
+                  type="text"
+                  placeholder="Enter number"
+                  onChange={(e) => {
+                    setAmount(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="left-div-plan">
+              <div className="radio-plan">
+                <div className="label">
+                  <label>Amount of hint</label>
+                  <div className="radio-radio">
+                    <input
+                      type="checkbox"
+                      value="color"
+                      name="art"
+                      className="checkbox"
+                      onClick={() => {
+                        setUnlimtedHints(!unlimtedHints);
+                      }}
+                    />{' '}
+                    Unlimited
+                  </div>
+                </div>
+                <input
+                  type="text"
+                  placeholder={unlimtedHints ? 'Unlimited' : 'Eg. 400'}
+                  onChange={(e) => {
+                    setNumOfHint(e.target.value);
+                  }}
+                  disabled={unlimtedHints}
+                  required
+                />
+              </div>
+              <div className="upload-plans">
+                <label>
+                  Choose the plan avatar
+                  <label className="plan-label">
+                    Choose a files
+                    <input
+                      type={'file'}
+                      placeholder="Choose a file"
+                      onInput={displayImage}
+                      onChange={imageUpload}
+                    />
+                  </label>
+                </label>
+                {image && (
+                  <img
+                    style={{ width: '241px', height: ' 270px' }}
+                    src={image}
+                    alt=""
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-      <button className="plan-button" onClick={createSubscription}>
-        Create Ad
-      </button>
+          <button className="plan-button" onClick={createSubscription}>
+            Create Ad
+          </button>
+        </form>
+      )}
+      {modalOpen && (
+        <Modal
+          setOpenModal={setModalOpen}
+          Modal={modalOpen}
+          subName={planName}
+        />
+      )}
     </div>
   );
 };
