4a1e9678cca2d258b6ac95328ea2caa5d28745ca
Uche Omodu <chivoomodu@gmail.com>
2022-06-17 17:52:29 +0100

    bug fixes

--- src/components/Achievements/Root/Root.tsx
+++ src/components/Achievements/Root/Root.tsx
@@ -1,36 +1,57 @@
-import { mockdata } from './mockdata';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import './root.scss';
-
-const LIMIT = 9;
+import { UserContext } from '../../../UserContext';
+import Loader from '../../../Loader/Loader';
 
 const Root = () => {
-  let listed = mockdata.slice(0, LIMIT);
+  const { token } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        'https://colorculture.herokuapp.com/achievements/',
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      );
+      setData(response.data.data);
+      setIsLoading(false);
+      console.log(response);
+    };
+    fetchData();
+  }, [token]);
 
   return (
     <>
-      <div className={'scroll-root-root '}>
-        {listed.map((item, index) => {
-          return (
-            <div className="ach-root" key={index}>
-              {/* <div> */}
-              <img
-                src={require('../../../assets/smartphone-addiction-line-icon-vector-37660174.png')}
-                alt=""
-              />
-              {/* </div> */}
-              <div className="ach-root2">
-                <div className="titleXimage">
-                  <p className="item-title">{item.title}</p>
-                </div>
-                <p className="item-desc">{item.description}</p>
-                <div className="points-root">
-                  <p className="item-points">{item.points}</p>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className={'scroll-root-root '}>
+          {data.map((item, index) => {
+            return (
+              <div className="ach-root" key={index}>
+                {/* <div> */}
+                <img src={item.icon_image} alt="" />
+                {/* </div> */}
+                <div className="ach-root2">
+                  <div className="titleXimage">
+                    <p className="item-title">{item.name}</p>
+                  </div>
+                  <p className="item-desc">{item.task}</p>
+                  <div className="points-root">
+                    <p className="item-points">{item.criteria}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
