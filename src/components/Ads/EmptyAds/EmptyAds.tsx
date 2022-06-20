4cc2ccebf5db1c3a071c22452c35a76e83415c5e
Uche Omodu <chivoomodu@gmail.com>
2022-06-20 14:28:07 +0100

    Empty Ads

---
+++ src/components/Ads/EmptyAds/EmptyAds.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmptyAds = () => {
+  return (
+    <div className="empty-ads">
+      <p>😊 PLEASE SELECT AN AD TO EDIT</p>
+    </div>
+  );
+};
+
+export default EmptyAds;
