d509d8e0a55b889fb186be947f93d38e6fa06bf5
Uche Omodu <chivoomodu@gmail.com>
2022-06-20 13:30:40 +0100

    Schema for Edit Achievements

---
+++ src/components/Achievements/EditAchievement/EditSchema.tsx
@@ -0,0 +1,25 @@
+import * as yup from 'yup';
+
+export const schema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  task: yup.string().required('Description is required'),
+  criteria: yup
+    .number()
+    .positive()
+    .integer()
+    .typeError('Criteria must be a number')
+    .required('Criteria is required'),
+  icon_image: yup.mixed().test('fileSize', 'The file is too large', (value) => {
+    return value && value[0] && value[0].size <= 200000;
+  }),
+  colored_icon_image: yup
+    .mixed()
+    .test('fileSize', 'The file is too large', (value) => {
+      return value && value[0] && value[0].size <= 200000;
+    }),
+  dark_icon_image: yup
+    .mixed()
+    .test('fileSize', 'The file is too large', (value) => {
+      return value && value[0] && value[0].size <= 200000;
+    }),
+});
