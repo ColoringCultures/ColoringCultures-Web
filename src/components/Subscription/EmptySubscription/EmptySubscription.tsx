63c21ed359a6e99819d413d6caf1cf0f2aac6a90
Uche Omodu <chivoomodu@gmail.com>
2022-06-20 18:23:35 +0100

    updating Empty component

---
+++ src/components/Subscription/EmptySubscription/EmptySubscription.tsx
@@ -0,0 +1,13 @@
+import './EmptySubscription.scss';
+
+const EmptySubscription = () => {
+  return (
+    <div className="empty-Subscription-root">
+      <div className="empty-Subscription">
+        <p>😊 PLEASE SELECT A SUBSCRIPTION TO EDIT</p>
+      </div>
+    </div>
+  );
+};
+
+export default EmptySubscription;
