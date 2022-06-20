3064615586bb861c5b25611daff35eb5edcfe7ef
Uche Omodu <chivoomodu@gmail.com>
2022-06-20 15:49:56 +0100

    Building container for Empty Ads

--- src/components/Ads/EmptyAds/EmptyAds.tsx
+++ src/components/Ads/EmptyAds/EmptyAds.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import './EmptyAds.scss';
 
 const EmptyAds = () => {
   return (
-    <div className="empty-ads">
-      <p>😊 PLEASE SELECT AN AD TO EDIT</p>
+    <div className="empty-ads-root">
+      <div className="empty-ads">
+        <p>😊 PLEASE SELECT AN AD TO EDIT</p>
+      </div>
     </div>
   );
 };
