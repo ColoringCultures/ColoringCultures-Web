37be6b1027275c503b09c99faae7ccf5056eb7f3
Uche Omodu <chivoomodu@gmail.com>
2022-06-21 15:07:37 +0100

    building mobile navbar

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
 import './Dashboard.scss';
 import { Outlet } from 'react-router-dom';
-import NavLink from '../../Navlink';
 import Modal from './Modal/Modal';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ImageIcon from '@mui/icons-material/Image';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
-import MessageIcon from '@mui/icons-material/Message';
-import PostAddIcon from '@mui/icons-material/PostAdd';
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import PaidIcon from '@mui/icons-material/Paid';
+import MediaQuery from 'react-responsive';
+import DesktopNavbar from './DesktopNavbar/DesktopNavbar';
+import TabNavbar from './TabNavbar/TabNavbar';
 
 const Dashboard = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -18,128 +12,12 @@ const Dashboard = () => {
   return (
     <div className="dashboard-root">
       <div className="navbar-root">
-        <div className="navbar">
-          <div className="admin-details">
-            <img src={require('../../assets/Ellipse 1.png')} alt="" />
-            <h1>Omodu Uche</h1>
-            <p>Admin</p>
-          </div>
-          <div className="menu-wrapper">
-            <h1 className="menu">Menu</h1>
-            <div className="links">
-              <div>
-                <NavLink
-                  inactiveClassName="navlink"
-                  to="/Dashboard/"
-                  exact={false}
-                  className="navlink"
-                  activeClassName="active"
-                >
-                  <DashboardIcon className="dashicon" />
-                  Dashboard
-                </NavLink>
-              </div>
-              <div>
-                <NavLink
-                  inactiveClassName="navlink"
-                  to="/Dashboard/Images"
-                  exact={false}
-                  className="navlink"
-                  activeClassName="active"
-                >
-                  <ImageIcon className="dashicon" />
-                  Images
-                </NavLink>
-              </div>
-              <div>
-                <NavLink
-                  inactiveClassName="navlink"
-                  to="/Dashboard/Statistics"
-                  exact={false}
-                  className="navlink"
-                  activeClassName="active"
-                >
-                  <BarChartIcon className="dashicon" />
-                  Statistics
-                </NavLink>
-              </div>
-              <div>
-                <NavLink
-                  inactiveClassName="navlink"
-                  to="/Dashboard/Achievements"
-                  exact={false}
-                  className="navlink"
-                  activeClassName="active"
-                >
-                  <EmojiEventsIcon className="dashicon" />
-                  Achievements
-                </NavLink>
-              </div>
-              <div>
-                <NavLink
-                  inactiveClassName="navlink"
-                  to="/Dashboard/UserFeedback"
-                  exact={false}
-                  className="navlink"
-                  activeClassName="active"
-                >
-                  <MessageIcon className="dashicon" />
-                  Users Feedback
-                </NavLink>
-              </div>
-              <div>
-                <NavLink
-                  inactiveClassName="navlink"
-                  to="/Dashboard/Ads"
-                  exact={false}
-                  className="navlink"
-                  activeClassName="active"
-                >
-                  <PostAddIcon className="dashicon" />
-                  Ads
-                </NavLink>
-              </div>
-              <div>
-                <NavLink
-                  inactiveClassName="navlink"
-                  to="/Dashboard/Subscription"
-                  exact={false}
-                  className="navlink"
-                  activeClassName="active"
-                >
-                  <SubscriptionsIcon className="dashicon" />
-                  Subscription
-                </NavLink>
-              </div>
-              <div>
-                <NavLink
-                  inactiveClassName="navlink"
-                  to="/Dashboard/Revenue"
-                  exact={false}
-                  className="navlink"
-                  activeClassName="active"
-                >
-                  <PaidIcon className="dashicon" />
-                  Revenue
-                </NavLink>
-              </div>
-            </div>
-            <div
-              className="link"
-              onClick={() => {
-                setModalOpen(true);
-              }}
-            >
-              <img
-                className="signout-icon"
-                src={require('../../assets/Path 29.png')}
-                alt=""
-              />
-              <p>Sign Out</p>
-            </div>
-            {/* <div></div> */}
-          </div>
-        </div>
+        <MediaQuery minWidth={992}>
+          <DesktopNavbar setModalOpen={setModalOpen} />
+        </MediaQuery>
+        <MediaQuery maxWidth={992}>
+          <TabNavbar setModalOpen={setModalOpen} />
+        </MediaQuery>
         {modalOpen && <Modal setOpenModal={setModalOpen} />}
       </div>
       <div className="outlet-div">
---
+++ src/pages/Dashboard/TabNavbar/TabNavbar.tsx
@@ -0,0 +1,130 @@
+import './TabNavbar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ImageIcon from '@mui/icons-material/Image';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import MessageIcon from '@mui/icons-material/Message';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import PaidIcon from '@mui/icons-material/Paid';
+import NavLink from '../../../Navlink';
+
+const TabNavbar = (setModalOpen: any) => {
+  return (
+    <div className="tab-navbar">
+      <div className="tab-admin-details">
+        <img src={require('../../../assets/Ellipse 1.png')} alt="" />
+        <h1>Omodu Uche</h1>
+        <p>Admin</p>
+      </div>
+      <div className="tab-menu-wrapper">
+        <h1 className="tab-menu">Menu</h1>
+        <div className="tab-links">
+          <div>
+            <NavLink
+              inactiveClassName="tab-navlink"
+              to="/Dashboard/"
+              exact={false}
+              className="tab-navlink"
+              activeClassName="tab-active"
+            >
+              <DashboardIcon className="tab-dashicon" />
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              inactiveClassName="tab-navlink"
+              to="/Dashboard/Images"
+              exact={false}
+              className="tab-navlink"
+              activeClassName="tab-active"
+            >
+              <ImageIcon className="tab-dashicon" />
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              inactiveClassName="tab-navlink"
+              to="/Dashboard/Statistics"
+              exact={false}
+              className="tab-navlink"
+              activeClassName="tab-active"
+            >
+              <BarChartIcon className="tab-dashicon" />
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              inactiveClassName="tab-navlink"
+              to="/Dashboard/Achievements"
+              exact={false}
+              className="tab-navlink"
+              activeClassName="tab-active"
+            >
+              <EmojiEventsIcon className="tab-dashicon" />
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              inactiveClassName="tab-navlink"
+              to="/Dashboard/UserFeedback"
+              exact={false}
+              className="tab-navlink"
+              activeClassName="tab-active"
+            >
+              <MessageIcon className="tab-dashicon" />
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              inactiveClassName="tab-navlink"
+              to="/Dashboard/Ads"
+              exact={false}
+              className="tab-navlink"
+              activeClassName="tab-active"
+            >
+              <PostAddIcon className="tab-dashicon" />
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              inactiveClassName="tab-navlink"
+              to="/Dashboard/Subscription"
+              exact={false}
+              className="tab-navlink"
+              activeClassName="tab-active"
+            >
+              <SubscriptionsIcon className="tab-dashicon" />
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              inactiveClassName="tab-navlink"
+              to="/Dashboard/Revenue"
+              exact={false}
+              className="tab-navlink"
+              activeClassName="tab-active"
+            >
+              <PaidIcon className="tab-dashicon" />
+            </NavLink>
+          </div>
+        </div>
+        <div
+          className="tab-link"
+          onClick={() => {
+            setModalOpen(true);
+          }}
+        >
+          <img
+            className="tab-signout-icon"
+            src={require('../../../assets/Path 29.png')}
+            alt=""
+          />
+        </div>
+        {/* <div></div> */}
+      </div>
+    </div>
+  );
+};
+
+export default TabNavbar;
