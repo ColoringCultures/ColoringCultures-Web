fdf10f5649d67ab75981bacee9cbfc9534ef06ce
Uche Omodu <79480742+qgatssdev@users.noreply.github.com>
2022-08-08 13:39:12 +0100

    Development (#105)
    * building achievement root
    
    * editing ads page
    
    * Building Create page
    
    * typo fix
    
    * adding create achivement styles
    
    * Styling Edit
    
    * advertisement schema
    
    * testing changes
    
    * Creating achivement schema
    
    * commenting out scss
    
    * Making changes to ads schema
    
    * Building Create page
    
    * Creating Loader component
    
    * Modal for Create Achievement
    
    * File schema
    
    * Finishing up Create page
    
    * Finshing Ads page
    
    * adding hover for buttons
    
    * adding hover for buttons
    
    * hover for button
    
    * Building Feedback page with endpoint
    
    * adding hover to button
    
    * changes to reset function
    
    * chnages to min width
    
    * changes to height of container
    
    * Changes to reset function
    
    * deleting subscheme
    
    * Finishing edit plans
    
    * Finishing Ad plans
    
    * styling change
    
    * deleting schema
    
    * typo error in component
    
    * Building subpage with api integratoin
    
    * building
    
    * updating loadmore function
    
    * fixing bug
    
    * updates
    
    * fixing bugs
    
    * updating styling
    
    * Update AddPlans.tsx
    
    * Modal for create achievement
    
    * finishing up
    
    * styling change
    
    * edits to add page
    
    * building sub page
    
    * bug fixes
    
    * styling fixes
    
    * I've sha done it
    
    * typo fix
    
    * Schema for Edit Achievements
    
    * Delete modal
    
    * Confirm Modal created
    
    * Edit achievement finished
    
    * styling fixes
    
    * timeout increased
    
    * EditEmpty page created
    
    * Empty Ads
    
    * Creating routes
    
    * Styling fix
    
    * typo fix
    
    * Building container for Empty Ads
    
    * Confirm modal built
    
    * Confirm modal built
    
    * Edit ads built
    
    * Dashboard position fix
    
    * updating Empty component
    
    * Fixed all fixables
    
    * bug fixes
    
    * installing icons
    
    * building mobile navbar
    
    * Styling for tab and ipad view
    
    * Catching network errors for all fetchs
    
    * Editing modal and fixing subscription bug
    
    * ads fix
    
    * root ads fix
    
    * ads fix
    
    * fixing root ads
    
    * adding network error catch
    
    * fixing width issues
    
    * Creating conversion file
    
    * Edit achievement fixing
    
    * Building revenue page
    
    * Building Statistics page
    
    * Building home dashboard
    
    * login-route
    
    * Building home page and Statistics
    
    * Building Statistics
    
    * building
    
    * Installing Charts
    
    * Building revenue and home
    
    * fixes for revenue and Stats page
    
    * Restyling for Visitor and Sub components
    
    * Saving Username in context
    
    * Rebuilding dashboard root page
    
    * Mobile view for home root page
    
    * Fixing Ads page
    
    * changing label direction
    
    * fixing update issue
    
    * Lots of changes
    
    * Fixing UserFeedback total number
    
    * changes made
    
    * fixing width
    
    * Building Home page
    
    * fixing achievement bug
    
    * fixes
    
    * fixes
    
    * fixes
    
    * sub fixes
    
    * adding default user image
    
    * building images root
    
    * empty images
    
    * work well done
    
    * big big fixes
    
    * removing trash
    
    * Categories added
    
    * fixes
    
    * so many fixes
    
    * working and working
    
    * lots of changes
    
    * fixes carried
    
    * changing of url
    
    * changes
    
    * changes

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,26 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Carousel.scss';
 import { mockdata } from './mockdata';
 import Loader from '../../Loader/Loader';
 import { useNavigate } from 'react-router-dom';
+import { url } from '../../api';
+import axios from 'axios';
+import { UserContext } from '../../UserContext';
 
 const Carousel = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [errMessage, setErrMessage] = useState('');
   const navigate = useNavigate();
   const [data, setData] = useState<any[]>(mockdata);
   const [dataList, setDataList] = useState<any[]>([]);
   const LIMIT = 4;
+  const { token } = useContext(UserContext);
   const LENGTH = dataList.length;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(`${url}/carousels/`, {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        })
+        .then((response) => {
+          setData(response.data.data);
+          console.log(response);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setErrMessage(err.message);
+          setIsLoading(false);
+        });
+    };
+    fetchData();
+  }, [token]);
 
   useEffect(() => {
     setDataList(data);
     setList(data.slice(0, LIMIT));
-    // remeber to remove this
-    setErrMessage('');
-    setIsLoading(false);
-    setData(data);
-    // this is for testing
   }, [LENGTH, data]);
 
   const [index, setIndex] = useState(LIMIT);
@@ -49,7 +69,7 @@ const Carousel = () => {
               return (
                 <div key={index} className="carousel-border">
                   <img
-                    src={data.file}
+                    src={data.image}
                     alt=""
                     className="carousel-border__image"
                   />
